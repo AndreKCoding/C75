659cc2a76378454c3e86865d6dae8cf28761509d
AndreKCoding <131627515+AndreKCoding@users.noreply.github.com>
2023-10-22 17:48:19 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHqAneSwOZE2O5Ot7xAsV_0HgwBq-dVbI",
+  authDomain: "projeto75-e7362.firebaseapp.com",
+  projectId: "projeto75-e7362",
+  storageBucket: "projeto75-e7362.appspot.com",
+  messagingSenderId: "425225663340",
+  appId: "1:425225663340:web:9d3beb4823fd2ce6f5b949"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
